066430c23a1c9b7aac1dac874a98af3fb006f2f9
Bradlee Harrison <bradlee.harrison22@gmail.com>
2023-10-11 21:31:45 -0500

    Fluid License

--- src/pages/fluid.js
+++ src/pages/fluid.js
@@ -1,3 +1,13 @@
+/*
+Copyright (c) 2023 by kynd (https://codepen.io/kynd/pen/jOXZXWB)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 import React, { useEffect, useRef } from 'react';
 
 function Animation() {
@@ -21,11 +31,30 @@ function Animation() {
         container.appendChild(canvas);
         container.appendChild(titleDiv);
 
-        let mapResolution = 10000;
-        let colorMap = [];
-        let velocityMap = [];
-        let sampleMap = [];
+        let colorMap, velocityMap;
+        let mapDisplaySize, mapResolution, gridSize;
 
+        function setup() {
+            frameRate(5);
+            // Initialize canvas and map settings
+            createCanvas(windowWidth, windowHeight)
+            mapDisplaySize = Math.min(width, height);
+            mapResolution = 48;
+            gridSize = mapDisplaySize / mapResolution;
+
+            initialize();
+        }
+
+        function mousePressed() {
+            // On mouse press, re-render
+            initialize();
+        }
+
+        function draw() {
+            background(255);
+            const xOffset = (width - mapDisplaySize) / 2;
+            const yOffset = (height - mapDisplaySize) / 2;
+        }
         function updateColorMap() {
             const newColorMap = [];
             for (let y = 0; y < mapResolution; y++) {
